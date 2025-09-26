fb3c095db856dfaf19b0dd6ed6ce8bf34248dd0a
Marcus Holmgren <marcus.holmgren1@gmail.com>
2025-09-26 07:29:32 +0200

    Switch Keycloak client to confidential with secret auth

--- get_token.sh
+++ get_token.sh
@@ -2,6 +2,7 @@
 curl --location 'http://localhost:8080/realms/contacts/protocol/openid-connect/token' \
 --header 'Content-Type: application/x-www-form-urlencoded' \
 --data-urlencode 'client_id=contacts-app-client' \
+--data-urlencode 'client_secret=frontend-secret' \
 --data-urlencode 'username=testuser' \
 --data-urlencode 'password=testpassword' \
 --data-urlencode 'grant_type=password' \
